fac5e94edafded55ff2297c224da5fe05738a42e
Bradford Ware <bware@RSG4689fordWare.rsg.lan>
2016-01-29 17:56:40 -0500

    added the akka streams pipeline

---
+++ src/main/scala/Actors.scala
@@ -0,0 +1,55 @@
+import akka.actor.{ Props, Actor }
+import akka.stream.actor.ActorPublisher
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import scala.collection.JavaConversions._
+
+// Data Structure to hold the message data
+case class EmailMessage(message: String)
+
+// Actor 1 ----> Actor 2 -----> Actor 3 -----> Dumps to console
+// Actor 1
+class EmailActor extends Actor {
+  val processor = context.system.actorOf(Props[EmailProcessor], "EmailProcessor")
+
+  def receive = {
+    case msg: EmailMessage => processor ! msg
+  }
+}
+
+// Actor 2
+class EmailProcessor extends Actor {
+  val printer = context.system.actorOf(Props[EmailPrinter], "EmailPrinter")
+
+  def receive = {
+    case EmailMessage(mail) => printer ! EmailMessage(processMessage(mail)) // Send the current greeting back to the sender
+  }
+
+  def processMessage(message: String) = {
+    message.toUpperCase.reverse
+  }
+}
+
+// Actor 3
+class EmailPrinter extends Actor {
+  def receive = {
+    case EmailMessage(message) => println(message)
+  }
+}
+
+/* Actor for the Akka Stream */
+class TwitterActor(consumer: KafkaConsumer[String, String]) extends ActorPublisher[String] {
+  consumer.subscribe(List("twitter-mailchimp")) //Kafka-Consumer reading from the topic new-test
+
+  def receive = {
+    case msg => println(msg)
+  }
+
+  def grabMessages = {
+    consumer.poll(100);
+  }
+}
+
+object TwitterActor {
+  def props(consumer: KafkaConsumer[String, String]): Props = Props(new TwitterActor(consumer))
+}
+
---
+++ src/main/scala/StreamMain.scala
@@ -0,0 +1,36 @@
+import java.util.Properties
+
+import akka.actor.{ActorSystem}
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{Sink, Source}
+import org.apache.kafka.clients.consumer.KafkaConsumer
+
+object StreamMain extends App {
+  implicit val system = ActorSystem("Twitter-MailChimp")
+  implicit val materializer = ActorMaterializer()
+
+  val props = new Properties()
+  props.put("bootstrap.servers", "localhost:9092")
+  props.put("group.id", "test")
+  props.put("enable.auto.commit", "true")
+  props.put("auto.commit.interval.ms", "1000")
+  props.put("session.timeout.ms", "30000")
+  props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+  props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+
+  val consumer = new KafkaConsumer[String, String](props)
+
+  val twitterSource = Source.actorPublisher[String](TwitterActor.props(consumer))
+  val consoleSink = Sink.foreach[String](println)
+
+  val runnableGraph = twitterSource
+    // transform message to upper-case
+    .map(msg => msg.toUpperCase)
+    // transform message to reverse value
+    .map(msg => msg.reverse)
+    // connecting to the sink
+    .to(consoleSink)
+
+  println("pipeline-demo starting...")
+  runnableGraph.run()
+}
